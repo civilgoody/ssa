31b5180fa062df79538318a9a52ea18a58aa8eba
goody <civilgoody@gmail.com>
2026-03-05 12:02:09 +0100

    chore: rename app and update app.json, package.json; remove package-lock.json

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-3xl font-bold underline">Home screen.</Text>
+      <Text className="text-3xl font-bold underline">Home screen</Text>
       <Link href="/about" className="text-blue-500 text-2xl">
         About
       </Link>
